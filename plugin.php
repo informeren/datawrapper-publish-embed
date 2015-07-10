1251d771707ee6f54b538fe6d5c6933960d6a14a
Morten Wulff <wulff@ratatosk.net>
2015-07-10 15:29:52 +0200

    Made title L10N-friendly

--- plugin.php
+++ plugin.php
@@ -1,33 +1,25 @@
 <?php
 
 class DatawrapperPlugin_PublishEmbed extends DatawrapperPlugin {
-
     public function init() {
         $plugin = $this;
+
         // hook into chart publication
         DatawrapperHooks::register(DatawrapperHooks::GET_CHART_ACTIONS, function() use ($plugin) {
             // no export possible without email
             $cfg = $plugin->getConfig();
             return array(
                 'id' => 'publish-embed',
-                'title' => 'Gerar código do spiffy',
+                'title' => __('Embed this chart'),
                 'icon' => 'code',
                 'order' => 150,
-                'banner' => array(
-                    'text' => '',
-                    'style' => ''
-                )
             );
         });
-
-
 
         // provide static assets files
         $this->declareAssets(
             array('publish-embed.js'),
             "|/chart/[^/]+/publish|"
         );
-
     }
-
 }
